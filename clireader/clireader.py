2e2829a51be57514313a195d6d3b741762fac53f
Paul J. Iutzi <pji@mac.com>
2022-08-27 09:27:25 -0500

    Initial implementation of Page.

--- clireader/clireader.py
+++ clireader/clireader.py
@@ -1,4 +1,186 @@
 """
 clireader
 ~~~~~~~~~
+
+A module for paging through text in the terminal.
 """
+from textwrap import wrap
+from typing import Generator, Optional
+
+from blessed import Terminal
+
+
+class Box:
+    """A class to track the characters used to draw a box in a
+    terminal.
+
+    It has fifteen properties that return the character used for
+    that part of the box:
+
+    * top: The top
+    * bot: The bottom
+    * side: The sides
+    * mhor: Interior horizontal lines
+    * mver: Interior vertical lines
+    * ltop: The top-left corner
+    * mtop: Top mid-join
+    * rtop: The top-right corner
+    * lside: Left side mid-join
+    * mid: Interior join
+    * rside: Right side mid-join
+    * lbot: Bottom-left corner
+    * mbot: Bottom mid-join
+    * rbot: Bottom-right corner
+    """
+    def __init__(self, kind: str = 'light', custom: str = None) -> None:
+        self._names = [
+            'top', 'bot', 'side',
+            'mhor', 'mver',
+            'ltop', 'mtop', 'rtop',
+            'lside', 'mid', 'rside',
+            'lbot', 'mbot', 'rbot',
+        ]
+        self._light = '──│─│┌┬┐├┼┤└┴┘'
+        self._heavy = '━━┃━┃┏┳┓┣╋┫┗┻┛'
+        self._light_double_dash = '╌╌╎╌╎' + self._light[3:]
+        self._heavy_double_dash = '╍╍╏╍╏' + self._heavy[3:]
+        self._light_triple_dash = '┄┄┆┄┆' + self._light[3:]
+        self._heavy_triple_dash = '┅┅┇┅┇' + self._heavy[3:]
+        self._light_quadruple_dash = '┈┈┊┈┊' + self._light[3:]
+        self._heavy_quadruple_dash = '┉┉┋┉┋' + self._heavy[3:]
+        if kind:
+            self.kind = kind
+        else:
+            self.kind = 'light'
+        if custom:
+            self.custom = custom
+            self.kind = 'custom'
+
+    def __getattr__(self, name):
+        try:
+            index = self._names.index(name)
+        except ValueError:
+            return self.__dict__[name]
+        return self._chars[index]
+
+    @property
+    def kind(self):
+        return self._kind
+
+    @kind.setter
+    def kind(self, value):
+        self._kind = value
+        self._chars = getattr(self, f'_{value}')
+
+    @property
+    def custom(self):
+        return self._custom
+
+    @custom.setter
+    def custom(self, value):
+        strvalue = str(value)
+        if len(strvalue) == 14:
+            self._custom = str(strvalue)
+            self._kind = 'custom'
+        else:
+            reason = 'The custom string must be 14 characters.'
+            raise ValueError(reason)
+
+
+class Page:
+    """View pages of text in a terminal."""
+    def __init__(
+            self,
+            text: str = '',
+            title: str = '',
+            frame: str = 'light',
+            padding: int = 1,
+            term: Optional[Terminal] = None
+    ) -> None:
+        self.frame = Box(kind=frame)
+        self.padding = padding
+        self.text = text
+        self.title = title
+        self.current_page = 0
+        if not term:
+            self.term = Terminal()
+
+    @property
+    def pages(self) -> list[list[str]]:
+        if '_pages' not in self.__dict__:
+            width = self.term.width + 1 - 2 - self.padding * 2
+            lines_per_page = self.term.height + 1 - 2 - self.padding * 2
+            wrapped = wrap(self.text, width)
+            self._pages = []
+            for i in range(0, len(wrapped), lines_per_page):
+                self._pages.append(wrapped[i: i + lines_per_page])
+        return self._pages
+
+    def _draw_command_hints(self) -> None:
+        hints = []
+        if len(self.pages) > 1:
+            hints.append(self.term.reverse + '>' + self.term.reverse + 'Next')
+        if len(self.pages) > 1 and self.current_page != 0:
+            hints.append(self.term.reverse + '<' + self.term.reverse + 'Back')
+        for i, hint in enumerate(hints):
+            y = self.term.height
+            x = self.term.width - 5 - 3 - 6 * i
+            print(self.term.move(y, x) + hint)
+
+    def _draw_frame(self) -> None:
+        """Draw the frame around the page."""
+        top = (
+            self.frame.ltop
+            + self.frame.top
+            + self.title
+            + self.frame.top * (self.term.width - len(self.title) - 3)
+            + self.frame.rtop
+        )
+        mid = (
+            self.frame.mver
+            + (' ' * (self.term.width - 2))
+            + self.frame.mver
+        )
+        bot = (
+            self.frame.lbot
+            + (self.frame.bot * (self.term.width - 2))
+            + self.frame.rbot
+        )
+
+        print(self.term.move(0, 0) + top)
+        for y in range(1, self.term.height):
+            print(self.term.move(y, 0) + mid)
+        print(self.term.move(self.term.height, 0) + bot)
+
+    def back(self) -> None:
+        """Go back to the previous page of text."""
+        self.current_page -= 1
+        self.draw()
+
+    def draw(self) -> None:
+        """Draw the page of text."""
+        # Draw the frame.
+        self._draw_frame()
+        self._draw_command_hints()
+
+        # Draw the text.
+        text = self.pages[self.current_page]
+        y = self.padding + 1
+        x = self.padding + 1
+        for line in text:
+            print(self.term.move(y, x) + line)
+            y += 1
+
+    def load(self, text: str, title: str = '') -> None:
+        """Update the text pages being shown."""
+        self.text = text
+        self.title = title
+        self.current_page = 0
+        if '_pages' in self.__dict__:
+            del self._pages
+        self.draw()
+
+    def next(self) -> None:
+        """Advance to the next page of text."""
+        self.current_page += 1
+        self.draw()
--- tests/test_clireader.py
+++ tests/test_clireader.py
@@ -1,5 +1,379 @@
 """
 test_clireader
 ~~~~~~~~~~~~~~
+
+Unit tests for the clireader.clireader module.
 """
 import unittest as ut
+from unittest.mock import call, Mock, patch, PropertyMock
+
+from blessed import Terminal
+
+from clireader import clireader
+
+
+class PageTestCase(ut.TestCase):
+    topleft = '\x1b[1;2H'
+    bold = '\x1b[1m'
+    rev = '\x1b[7m'
+    loc = '\x1b[{};{}H'
+
+    """A UI for paging through text."""
+    def setUp(self):
+        self.term = Terminal()
+        self.term_h = 8
+        self.term_w = 14
+        self.text_long = 'This is longer text.'
+        self.text_overflow = (
+            'This text should over flow the very small box used for '
+            'testing. That is good. We want to use it to test how '
+            'the box handles over-flowing text.'
+        )
+        self.text_short = 'Eggs.'
+        self.title = 'spam'
+
+        # Common expected values.
+        self.draw_frame = [
+            call(self.loc.format(1, 1) + '┌─spam───────┐'),
+            call(self.loc.format(2, 1) + '│            │'),
+            call(self.loc.format(3, 1) + '│            │'),
+            call(self.loc.format(4, 1) + '│            │'),
+            call(self.loc.format(5, 1) + '│            │'),
+            call(self.loc.format(6, 1) + '│            │'),
+            call(self.loc.format(7, 1) + '│            │'),
+            call(self.loc.format(8, 1) + '│            │'),
+            call(self.loc.format(9, 1) + '└────────────┘'),
+        ]
+        self.draw_long = [
+            *self.draw_frame,
+            call(self.loc.format(3, 3) + 'This is'),
+            call(self.loc.format(4, 3) + 'longer'),
+            call(self.loc.format(5, 3) + 'text.'),
+        ]
+        self.draw_overflow = [
+            *self.draw_frame,
+            call(self.loc.format(9, 7) + self.rev + '>' + self.rev + 'Next'),
+            call(self.loc.format(3, 3) + 'This text'),
+            call(self.loc.format(4, 3) + 'should over'),
+            call(self.loc.format(5, 3) + 'flow the'),
+            call(self.loc.format(6, 3) + 'very small'),
+            call(self.loc.format(7, 3) + 'box used'),
+        ]
+        self.draw_overflow_pg2 = [
+            *self.draw_frame,
+            call(self.loc.format(9, 7) + self.rev + '>' + self.rev + 'Next'),
+            call(self.loc.format(9, 1) + self.rev + '<' + self.rev + 'Back'),
+            call(self.loc.format(3, 3) + 'for'),
+            call(self.loc.format(4, 3) + 'testing.'),
+            call(self.loc.format(5, 3) + 'That is'),
+            call(self.loc.format(6, 3) + 'good. We'),
+            call(self.loc.format(7, 3) + 'want to use'),
+        ]
+        self.draw_short = [
+            *self.draw_frame,
+            call(self.loc.format(3, 3) + self.text_short),
+        ]
+
+    # Test Page.__init__().
+    def test_init(self):
+        """When called, a Page() object is properly initialized and
+        returned.
+        """
+        exp_class = clireader.Page
+        exp_text = ''
+        act_obj = clireader.Page()
+        act_text = act_obj.text
+        self.assertIsInstance(act_obj, exp_class)
+        self.assertEqual(exp_text, act_text)
+
+    def test_text(self):
+        """When called with text, a Page() object is properly initialized
+        and returned.
+        """
+        exp_class = clireader.Page
+        exp_text = 'spam'
+        act_obj = clireader.Page(exp_text)
+        act_text = act_obj.text
+        self.assertIsInstance(act_obj, exp_class)
+        self.assertEqual(exp_text, act_text)
+
+    def test_set_frame(self):
+        """When initialized with a frame parameter, the page will store
+        the appropriate frame to use when drawing the page.
+        """
+        exp = 'heavy'
+        page = clireader.Page('spam', frame=exp)
+        act = page.frame.kind
+        self.assertEqual(exp, act)
+
+    def test_set_title(self):
+        """When initialized with a title parameter, the page will store
+        the title to use when displaying the page.
+        """
+        exp = 'eggs'
+        page = clireader.Page('spam', title=exp)
+        act = page.title
+        self.assertEqual(exp, act)
+
+    def test_set_padding(self):
+        """When initialized with a padding parameter, the page will store
+        the padding to use around the text when displaying the page.
+        """
+        exp = 2
+        page = clireader.Page('spam', padding=exp)
+        act = page.padding
+        self.assertEqual(exp, act)
+
+    # Test Page.back()
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_back(
+        self,
+        mock_print,
+        mock_height,
+        mock_width
+    ):
+        """When called, Page.back() should decrement the current page
+        and draw the page in the terminal.
+        """
+        # Expected value.
+        exp = [
+            call(self.loc.format(1, 1) + '┌─spam───────┐'),
+            call(self.loc.format(2, 1) + '│            │'),
+            call(self.loc.format(3, 1) + '│            │'),
+            call(self.loc.format(4, 1) + '│            │'),
+            call(self.loc.format(5, 1) + '│            │'),
+            call(self.loc.format(6, 1) + '│            │'),
+            call(self.loc.format(7, 1) + '│            │'),
+            call(self.loc.format(8, 1) + '│            │'),
+            call(self.loc.format(9, 1) + '└────────────┘'),
+            call(self.loc.format(9, 7) + self.rev + '>' + self.rev + 'Next'),
+            call(self.loc.format(3, 3) + 'This text'),
+            call(self.loc.format(4, 3) + 'should over'),
+            call(self.loc.format(5, 3) + 'flow the'),
+            call(self.loc.format(6, 3) + 'very small'),
+            call(self.loc.format(7, 3) + 'box used'),
+        ]
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_overflow,
+            title=self.title,
+            frame='light'
+        )
+        page.current_page = 1
+
+        # Run test and gather actuals.
+        page.back()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    # Test Page.draw()
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_draw(self, mock_print, mock_height, mock_width):
+        """When called, Page.draw() should draw the page in the terminal.
+        """
+        # Expected value.
+        exp = self.draw_short
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_short,
+            title=self.title,
+            frame='light'
+        )
+
+        # Run test and gather actuals.
+        page.draw()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_draw_text_wrap(self, mock_print, mock_height, mock_width):
+        """When called, Page.draw() should draw the page in the terminal.
+        """
+        # Expected value.
+        exp = self.draw_long
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_long,
+            title=self.title,
+            frame='light'
+        )
+
+        # Run test and gather actuals.
+        page.draw()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_draw_text_overflow(
+        self,
+        mock_print,
+        mock_height,
+        mock_width
+    ):
+        """When called, Page.draw() should draw the page in the terminal.
+        """
+        # Expected value.
+        exp = self.draw_overflow
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_overflow,
+            title=self.title,
+            frame='light'
+        )
+
+        # Run test and gather actuals.
+        page.draw()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_draw_text_overflow_page_2(
+        self,
+        mock_print,
+        mock_height,
+        mock_width
+    ):
+        """When called, Page.draw() should draw the page in the terminal.
+        """
+        # Expected value.
+        exp = self.draw_overflow_pg2
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_overflow,
+            title=self.title,
+            frame='light'
+        )
+        page.current_page = 1
+
+        # Run test and gather actuals.
+        page.draw()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    # Test Page.load()
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_load(self, mock_print, mock_height, mock_width):
+        """When called with new text and title, Page.load() should
+        update the text stored, reset the current page to zero, and
+        redraw the screen.
+        """
+        # Expected value.
+        exp = self.draw_overflow
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_short,
+            title='bacon',
+            frame='light'
+        )
+        _ = page.pages
+
+        # Run test and gather actuals.
+        page.load(text=self.text_overflow, title=self.title)
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_load_without_previous_draw(
+        self,
+        mock_print,
+        mock_height,
+        mock_width
+    ):
+        """When called without the Page having drawn to the screen before,
+        Page.load() should not error.
+        """
+        # Expected value.
+        exp = self.draw_overflow
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_short,
+            title='bacon',
+            frame='light'
+        )
+
+        # Run test and gather actuals.
+        page.load(text=self.text_overflow, title=self.title)
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
+
+    # Test Page.next()
+    @patch('clireader.clireader.Terminal.width', new_callable=PropertyMock)
+    @patch('clireader.clireader.Terminal.height', new_callable=PropertyMock)
+    @patch('clireader.clireader.print')
+    def test_next(
+        self,
+        mock_print,
+        mock_height,
+        mock_width
+    ):
+        """When called, Page.next() should increment the current page
+        and draw the page in the terminal.
+        """
+        # Expected value.
+        exp = self.draw_overflow_pg2
+
+        # Test data and state.
+        mock_height.return_value = self.term_h
+        mock_width.return_value = self.term_w
+        page = clireader.Page(
+            self.text_overflow,
+            title=self.title,
+            frame='light'
+        )
+        page.current_page = 0
+
+        # Run test and gather actuals.
+        page.next()
+        act = mock_print.mock_calls
+
+        # Determine test result.
+        self.assertListEqual(exp, act)
